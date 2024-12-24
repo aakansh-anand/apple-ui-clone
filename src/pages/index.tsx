873e09694bf0739e8e8aec775ec9f9d2c40f0806
Aakansh Anand <aakanshanand15@gmail.com>
2024-12-24 11:56:20 +0530

    Project Setup Complete

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,18 @@
+import Hero from "@/components/Hero";
+import Highlights from "@/components/Highlights";
+import Navbar from "@/components/Navbar";
+import Head from "next/head";
+
 const index = () => {
   return (
-    <div>
-      <h1 className="mt-10 text-center text-4xl font-semibold text-blue">
-        Hello World
-      </h1>
-    </div>
+    <main className="bg-black">
+      <Head>
+        <title>Apple iPhone</title>
+      </Head>
+      <Navbar />
+      <Hero />
+      <Highlights />
+    </main>
   );
 };
 export default index;
